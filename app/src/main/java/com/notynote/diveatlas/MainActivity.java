e752b1090711222c1a01ca399b57906f7196e557
notynote <epochmaking.th@gmail.com>
2019-08-20 11:45:58 +0700

    Apply Map in Map Page

--- app/src/main/java/com/notynote/diveatlas/MainActivity.java
+++ app/src/main/java/com/notynote/diveatlas/MainActivity.java
@@ -6,12 +6,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    FragmentManager fm = getSupportFragmentManager();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,16 +25,20 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    changeFrag(findViewById(R.id.navigation_home));
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_maps:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    changeFrag(findViewById(R.id.navigation_maps));
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_divesite:
-                    mTextMessage.setText(R.string.title_notifications);
+                    changeFrag(findViewById(R.id.navigation_divesite));
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText("Profile");
+                    changeFrag(findViewById(R.id.navigation_profile));
+                    //mTextMessage.setText("Profile");
                     return true;
             }
             return false;
@@ -43,6 +52,37 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void changeFrag(View v){
+        Fragment frag = null;
+        switch (v.getId()) {
+            case R.id.navigation_home:
+                frag = new Home();
+                break;
+            case R.id.navigation_maps:
+                frag = new Maps();
+                break;
+            case R.id.navigation_divesite:
+                frag = new DiveSite();
+                break;
+            case R.id.navigation_profile:
+                frag = new ProfilePage();
+                break;
+        }
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentView, frag);
+
+        //backstack check with back button on device
+        ft.addToBackStack("com.notynote.week6class.oneFragment");
+
+        //commit the change
+        ft.commit();
+
+
+
+
     }
 
 }
---
+++ app/src/main/java/com/notynote/diveatlas/Maps.java
@@ -0,0 +1,45 @@
+package com.notynote.diveatlas;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class Maps extends Fragment implements OnMapReadyCallback {
+
+    //Layout Variable
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+}
